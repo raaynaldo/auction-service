bef13ead4d52543d8869fe78c7200c5eb77cf4a9
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-09-06 17:54:37 -0400

    feat: update lib folder location

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import commonMiddleware from '../../lib/commonMiddleware';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -8,12 +8,15 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function createAuction(event, context) {
   const { title } = event.body;
   const now = new Date();
+  const endDate = new Date();
+  endDate.setHours(now.getHours() + 1);
 
   const auction = {
     id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
+    endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0,
     },
