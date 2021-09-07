91929cc0aef23ec0cab9e47d359f92d0222ad5db
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-09-06 22:11:48 -0400

    feat: validation for create auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,6 +2,8 @@ import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
+import validator from '@middy/validator';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -40,4 +42,8 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(
+  validator({
+    inputSchema: createAuctionSchema,
+  })
+);
