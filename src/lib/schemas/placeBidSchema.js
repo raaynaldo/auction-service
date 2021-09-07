17791c0948017132e6a336b4aed32fd8ebad439f
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-09-06 22:16:31 -0400

    feat: create placeBidSchema

---
+++ src/lib/schemas/placeBidSchema.js
@@ -0,0 +1,15 @@
+export default placeBidSchema = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        amount: {
+          type: 'number',
+        },
+      },
+      required: ['amount'],
+    },
+  },
+  required: ['body'],
+};
